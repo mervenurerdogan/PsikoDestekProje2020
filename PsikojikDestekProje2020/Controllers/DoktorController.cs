8f7497fd067918d8843c44ab75fbe8e290cfb4b3
Esra YÜZGEÇ <esrayuzgec0@gmail.com>
2020-10-09 23:34:31 +0300

    Hasta Doktor Layout eklendi Model eklendii

---
+++ PsikojikDestekProje2020/Controllers/DoktorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PsikojikDestekProje2020.Controllers
+{
+    public class DoktorController : Controller
+    {
+        // GET: Doktor
+        public ActionResult DoktorIndex()
+        {
+            return View();
+        }
+    }
+}
---
+++ PsikojikDestekProje2020/Controllers/HastaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PsikojikDestekProje2020.Controllers
+{
+    public class HastaController : Controller
+    {
+        // GET: Hasta
+        public ActionResult HAstaIndex()
+        {
+            return View();
+        }
+    }
+}
